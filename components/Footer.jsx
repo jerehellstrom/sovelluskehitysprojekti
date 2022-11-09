d7ea98a5d0527b8e1cdfebe42436efd3c22ddd1a
jerehellstrom <72606337+jerehellstrom@users.noreply.github.com>
2022-11-09 19:32:19 +0200

    header/footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,55 +1,42 @@
-import Image from "next/image";
-import styles from "../styles/Footer.module.css";
+import styles from "../styles/Footer.module.css"
+import Image from "next/image"
+
 
 const Footer = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.item}>
-        <Image src="/img/bg.png" objectFit="cover" layout="fill" alt="" />
-      </div>
-      <div className={styles.item}>
-        <div className={styles.card}>
-          <h2 className={styles.motto}>
-            OH YES, WE DID.THE LAMA PIZZA, WELL BAKED SLICE OF PIZZA.
-          </h2>
-        </div>
-        <div className={styles.card}>
-          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
-          <p className={styles.text}>
-            1654 R. Don Road #304.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1010
-          </p>
-          <p className={styles.text}>
-            2356 K. Laquie Rd #235.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1011
-          </p>
-          <p className={styles.text}>
-            1614 E. Erwin St #104.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1012
-          </p>
-          <p className={styles.text}>
-            1614 W. Caroll St #125.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1013
-          </p>
-        </div>
-        <div className={styles.card}>
-          <h1 className={styles.title}>WORKING HOURS</h1>
-          <p className={styles.text}>
-            MONDAY UNTIL FRIDAY
-            <br /> 9:00 – 22:00
-          </p>
-          <p className={styles.text}>
-            SATURDAY - SUNDAY
-            <br /> 12:00 – 24:00
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className={styles.container}>
+            <div className={styles.innerContainer}>
+                <div className={styles.appLinks}>
+                    <Image src="/img/logoruokamieles.png" alt="" width="160" height="50"/>
+                    </div>
+                    <div className={styles.appLinks}>
+                    <Image src="/img/logoappstorre.png" alt="" width="150" height="50"/>
+                    <Image src="/img/logoplaystorre.png" alt="" width="150" height="50"/>
+                    </div>
+                <div className={styles.info}></div>
+            </div>
 
-export default Footer;
+            <div className={styles.bottom}>
+                <div className={styles.bottomTiedot}>
+                    <div className={styles.tiedot}>
+                        <h1 className={styles.otsake}>Ravintolan sijainti</h1>
+                        <p className={styles.text}> kerhokuja 6 H <br/>Kekkola 01234<br/>+358 044 567893</p>
+                    </div>
+                    <div className={styles.tiedot}>
+                        <h1 className={styles.otsake}>Avoinna</h1>
+                        <p className={styles.text}>MA - PE <br/>11:00 - 23:00</p>
+                        <p className={styles.text}>LA & SU <br/>11:00 - 02:00</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className={styles.copyright}>
+                <p className={styles.text}>c ruokamieles 2022</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
